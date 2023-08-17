8a266346977c1837bcd6e5a087d46a17975e43b7
Minong <127920459+Minong26@users.noreply.github.com>
2023-08-17 11:41:25 +0900

    Player local movement

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 1.0f;
     void Start()
     {
-        Managers mg = Managers.GetInstance();
-        Debug.Log("mg : " +  mg);
+        //Managers mg = Managers.GetInstance();
+        //Debug.Log("mg : " +  mg);
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(h, 0, v).normalized;
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
