15c27ae49fd413dc253a0ecce8c2bb84005b1da6
Minong <127920459+Minong26@users.noreply.github.com>
2023-09-06 15:54:04 +0900

    UI Automatic

--- Assets/01_Scripts/01_Controllers/PlayerController.cs
+++ Assets/01_Scripts/01_Controllers/PlayerController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -12,8 +9,6 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        //Managers.Input.KeyAction -= OnKeyboard;
-        //Managers.Input.KeyAction += OnKeyboard;
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
@@ -59,33 +54,6 @@ public class PlayerController : MonoBehaviour
             _state = PlayerState.Running;
         }
     }
-
-    //private void OnKeyboard()
-    //{
-    //    if (Input.GetKey(KeyCode.W))
-    //    {
-    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-    //        transform.position += Vector3.forward * Time.deltaTime * speed;
-    //    }
-
-    //    if (Input.GetKey(KeyCode.S))
-    //    {
-    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-    //        transform.position += Vector3.back * Time.deltaTime * speed;
-    //    }
-
-    //    if (Input.GetKey(KeyCode.D))
-    //    {
-    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-    //        transform.position += Vector3.right * Time.deltaTime * speed;
-    //    }
-
-    //    if (Input.GetKey(KeyCode.A))
-    //    {
-    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-    //        transform.position += Vector3.left * Time.deltaTime * speed;
-    //    }
-    //}
 
     private void UpdateIdle()
     {
--- Assets/01_Scripts/02_UI/UI_Button.cs
+++ Assets/01_Scripts/02_UI/UI_Button.cs
@@ -1,16 +1,44 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Button : MonoBehaviour
 {
+    private Dictionary<Type, UnityEngine.Object[]> _object = new Dictionary<Type, UnityEngine.Object[]>();
+
+    enum Buttons
+    {
+        ScoreUp_btn
+    }
+    enum Texts
+    {
+        ScoreUpBtn_txt,
+        Score_txt,
+    }
+
     [SerializeField] private TextMeshProUGUI _text;
     private int _score = 0;
 
     private void Start()
     {
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
         _text.text = $"Score : {_score}";
+    }
+
+    private void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _object.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+        }
     }
 
     public void OnButtonClicked()
